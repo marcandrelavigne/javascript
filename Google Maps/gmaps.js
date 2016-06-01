55f279f1479fbd4dcb6444028452f2686258822e
marcandrelavigne <marcandre.lavigne@me.com>
2016-06-01 10:34:37 -0400

    Update gmaps.js

--- Google Maps/gmaps.js
+++ Google Maps/gmaps.js
@@ -1,16 +1,16 @@
 google.maps.event.addDomListener(window, 'load', init);
 function init() {
 	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	var isDraggable = w > 768 ? true : false;
-    var mapOptions = {
-        zoom: 16,
-	    draggable: isDraggable,
-        scrollwheel: false,
-        panControl: false,
-	    zoomControl: true,
-	    mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
+	var isDraggable = w > 768 ? true : false; // Si largeur de la page est plus grande que 768, activer draggable
+	var mapOptions = {
+		zoom: 16,
+		draggable: isDraggable,
+		scrollwheel: false,
+		panControl: false,
+		zoomControl: true,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
 		center: new google.maps.LatLng(45.3985693,-71.9327213),
 		styles: [ {
 			"featureType":"all",
@@ -23,12 +23,12 @@ function init() {
 			]
 		}]
 	};
-    var mapElement = document.getElementById('map');
-    var map = new google.maps.Map(mapElement, mapOptions);
-    var image = '/wp-content/themes/bravad/assets/img/map-pin.png';
-    var LocationMarker = new google.maps.Marker({
-        position: new google.maps.LatLng(45.3985693,-71.9327213),
-        map: map,
-        icon: image
-    });
+	var mapElement = document.getElementById('map');
+    	var map = new google.maps.Map(mapElement, mapOptions);
+    	var image = '/wp-content/themes/bravad/assets/img/map-pin.png';
+    	var LocationMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(45.3985693,-71.9327213),
+		map: map,
+		icon: image
+	});
 }
