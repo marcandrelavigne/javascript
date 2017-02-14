a003595a72f4f9becdb7bb2457d6982eb52f749b
marcandrelavigne <marcandre.lavigne@me.com>
2017-02-14 17:06:28 -0500

    Update old-android.js

--- Accessibility/old-android.js
+++ Accessibility/old-android.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function($) {	
-  var nua = navigator.userAgent.toLowerCase();
+	var nua = navigator.userAgent.toLowerCase();
 	var is_android = ((nua.indexOf('mozilla/5.0') > -1 && nua.indexOf('android ') > -1 && nua.indexOf('applewebkit') > -1) && !(nua.indexOf('chrome') > -1));
 	
-  // Add .oldandroid Class to body
-  if(is_android) {
-	     jQuery('body').addClass('oldandroid');
+	// Add .oldandroid Class to body
+	if(is_android) {
+		jQuery('body').addClass('oldandroid');
 	}
   
-  // Replace SVG for PNG
-  /*
+	// Replace SVG for PNG
+	/*
 	if(jQuery('body').hasClass('oldandroid')){
 		var new_src = jQuery('.logo img').attr('src').replace('.svg','.png');
 		jQuery('.logo img').attr('src',new_src);
 	}
-  */
+	*/
 
 });
