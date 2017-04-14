6f792a74d1e18a253040c972bea8b514159e8727
marcandrelavigne <marcandre.lavigne@me.com>
2017-04-14 10:50:50 -0400

    Add the User Agents

--- Accessibility/mobile-phone-number.js
+++ Accessibility/mobile-phone-number.js
@@ -1,4 +1,13 @@
-if(isMobile.phone) {
+// Disable automatic phone number detection on mobile devices, so the Fax numbers won't be clickable
+// Add this in the <head>: <meta name="format-detection" content="telephone=no" />
+
+// Define the user agent
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Trident/i.test(navigator.userAgent) ? true : false;
+
+// If on mobile device
+// Replace: <span class="phone-number">819-349-8888</span>
+// By: <a href="tel:819-349-8888">819-349-8888</a>
+if(isMobile) {
 	$('.phone-number').each(function() {
 		$(this).replaceWith(
 			$('<a href="tel:' + this.innerHTML + '">' + this.innerHTML + '</a>')
