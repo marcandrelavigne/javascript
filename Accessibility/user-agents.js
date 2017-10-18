5150aa1344fd14c01b5371e494b23bdbc10ae09f
marcandrelavigne <marcandre.lavigne@me.com>
2017-10-18 17:38:27 -0400

    Create user-agents.js

---
+++ Accessibility/user-agents.js
@@ -0,0 +1,32 @@
+// Desktop User Agent
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Trident/i.test(navigator.userAgent) ? true : false;
+if(!isMobile) {
+  jQuery('body').addClass('desktop');
+}
+
+// iOS User Agent
+if( navigator.userAgent.match(/iP(hone|od|ad)/i) ) {
+  jQuery('body').addClass('ios');
+
+	function iosVhHeightBug() {
+    var height = $(window).height();
+		$("#cssmenu ul.open").css('min-height', height);
+	  $(".slideshow").css('min-height', height);
+	}
+
+	iosVhHeightBug();
+	$(window).bind('resize', iosVhHeightBug);
+}
+
+var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+  jQuery('body').addClass('android');
+}
+
+// Old Android Fix
+var nua = navigator.userAgent.toLowerCase();
+var is_android = ((nua.indexOf('mozilla/5.0') > -1 && nua.indexOf('android ') > -1 && nua.indexOf('applewebkit') > -1) && !(nua.indexOf('chrome') > -1));
+if(is_android) {
+  jQuery('body').addClass('oldandroid');
+}
