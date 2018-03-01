ed6933bc74c8f6e5105ef85a30aa1053f667bb62
marcandrelavigne <marcandre.lavigne@me.com>
2018-03-01 16:58:07 -0500

    Create getAge.js

---
+++ jQuery/getAge.js
@@ -0,0 +1,15 @@
+;(function getAge($) {
+  function getAgeSince(dateString) {
+    var today = new Date();
+		var birthDate = new Date(dateString);
+		var age = today.getFullYear() - birthDate.getFullYear();
+		var m = today.getMonth() - birthDate.getMonth();
+		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+			age--;
+    }
+		return age;
+	};
+
+	$('.age').text('Since ' + getAgeSince("2014/06/21") + ' years');
+
+})(jQuery);
