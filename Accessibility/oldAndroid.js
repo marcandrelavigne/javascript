0ecb9847877a6501e9ca42d935d9b97c5f5e81fb
marcandrelavigne <marcandre.lavigne@me.com>
2018-03-01 12:09:32 -0500

    Update and rename old-android.js to oldAndroid.js

--- Accessibility/oldAndroid.js
+++ Accessibility/oldAndroid.js
@@ -4,12 +4,13 @@
 	Kitkat 4.4 and lower User Agent.
 
  */
+
 ;(function oldAndroid($) {
 
 	var nua = navigator.userAgent.toLowerCase();
 	var is_oldandroid = ((nua.indexOf('mozilla/5.0') > -1 && nua.indexOf('android ') > -1 && nua.indexOf('applewebkit') > -1) && !(nua.indexOf('chrome') > -1));
 
-	// Add .💩 Class to body
+	// Add Class to body
 	if(is_oldandroid) {
 		$('body').addClass('💩');
 	}
