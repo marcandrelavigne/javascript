daf06b886d3456cdc3e9c301db107a5a6529fbe9
marcandrelavigne <marcandre.lavigne@me.com>
2018-04-20 10:39:09 -0400

    Update img-to-inline-svg.js

--- jQuery/SVG/img-to-inline-svg.js
+++ jQuery/SVG/img-to-inline-svg.js
@@ -1,34 +1,40 @@
-/* ========================================================
-	Replace all SVG images with inline SVG
-	Add the class .svg to the img tag and voilà!
-========================================================= */
+/*
+	INLINE SVG
+	------
+	Replace images tags containing .svg with inline SVG
 
-jQuery('img.svg').each(function(){
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
+ */
 
-    jQuery.get(imgURL, function(data) {
+;inlineSvg = (function($) {
 
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
+	jQuery('img.svg').each(function(){
+	    var $img = jQuery(this);
+	    var imgID = $img.attr('id');
+	    var imgClass = $img.attr('class');
+	    var imgURL = $img.attr('src');
 
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
+	    jQuery.get(imgURL, function(data) {
 
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
+	        // Get the SVG tag, ignore the rest
+	        var $svg = jQuery(data).find('svg');
 
-        // Replace image with new SVG
-        $img.replaceWith($svg);
+	        // Add replaced image's ID to the new SVG
+	        if(typeof imgID !== 'undefined') {
+	            $svg = $svg.attr('id', imgID);
+	        }
+	        // Add replaced image's classes to the new SVG
+	        if(typeof imgClass !== 'undefined') {
+	            $svg = $svg.attr('class', imgClass+' replaced-svg');
+	        }
 
-    }, 'xml');
+	        // Remove any invalid XML tags as per http://validator.w3.org
+	        $svg = $svg.removeAttr('xmlns:a');
 
-});
+	        // Replace image with new SVG
+	        $img.replaceWith($svg);
+
+	    }, 'xml');
+
+	});
+
+})(jQuery);
